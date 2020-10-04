0c0f397be523c3a6480815a94274ada4660d1d78
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2020-10-04 20:01:12 +0530

    Weather App
    Changing the Setting.py file

--- Weather_App/settings.py
+++ Weather_App/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'abg743m!0ma_sdpg2$#4!zx+_je&w^om5pf9q0-6hcethlne&z'
+SECRET_KEY = 'SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
